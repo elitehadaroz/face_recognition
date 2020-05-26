d78d1d8ce070e16cfc91cc09d6166db285ce4b0b
elite <elitehadaroz@gmail.com>
2020-05-26 14:56:44 +0300

    loading the face detector and embedder from disk

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -21,3 +21,12 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+modelPath = os.path.sep.join([args["detector"],
+                              "res10_300x300_ssd_iter_140000.caffemodel"])
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
